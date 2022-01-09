061cf3e55cbb25fe31d127f491533732b2843ba9
harry <shsage91@gmail.com>
2022-01-10 01:32:38 +0900

    [feat] beforeEach & afterEach

--- packages/jest-101/src/basic-concept/Calculator.test.ts
+++ packages/jest-101/src/basic-concept/Calculator.test.ts
@@ -1,19 +1,28 @@
 import { Calculator } from "./Calculator"
 
-
 describe("calculator", () => {
   describe("should perform addition", () => {
+
+    // setup
+    let calculator: Calculator
+    beforeEach(() => {
+      calculator = new Calculator(2)
+    })
+    afterEach(() => {
+      console.log("some clean up task")
+    })
+
     it("adds two positive numbers", () => {
-      const calc = new Calculator(2)
-      const result = calc.add(1.333, 3.2)
+      const result = calculator.add(1.333, 3.2)
       expect(result).toEqual(4.53)
     })
+
     it("adds two negative numbers", () => {
-      const calc = new Calculator(2)
-      const result = calc.add(-1.333, -3.2)
+      const result = calculator.add(-1.333, -3.2)
       expect(result).toEqual(-4.53)
     })
   })
+
   it("should perform subtraction", () => {})
   it("should perform multiplication", () => {})
   it("should perform division", () => {})
