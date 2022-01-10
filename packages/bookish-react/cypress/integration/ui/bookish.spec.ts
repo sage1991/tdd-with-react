1f173167230603ffed40fe58469d0a59684f5bc6
harry <shsage91@gmail.com>
2022-01-10 18:52:34 +0900

    [feat] split tsconfig for cypress & refator code

--- packages/bookish-react/cypress/integration/ui/bookish.spec.ts
+++ packages/bookish-react/cypress/integration/ui/bookish.spec.ts
@@ -3,5 +3,16 @@ describe("Bookish application", () => {
     cy.visit("http://localhost:3000")
     cy.get("h2[data-test='heading']").contains("Bookish")
   })
+
+  it("Shows a book list", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("div[data-test='book-list']").should("exist")
+    cy.get("div.book-item").should("have.length", 2)
+    cy.get("div.book-item").should((books) => {
+      expect(books).to.have.length(2)
+      const titles = Array.prototype.map.call(books, (book: HTMLElement) => book.querySelector("h2").innerText)
+      expect(titles).to.deep.equal([ "Refactoring", "Domain-driven design" ])
+    })
+  })
 })
 
--- packages/bookish-react/src/App.tsx
+++ packages/bookish-react/src/App.tsx
@@ -1,12 +1,20 @@
 import React, { FC } from "react"
 import { Typography } from "@mui/material"
+import { BookList } from "./components/BookList"
+
 
 export const App: FC = (props) => {
+  const books = [
+    { name: "Refactoring" },
+    { name: "Domain-driven design" }
+  ]
+
   return (
     <div>
       <Typography variant="h2" component="h2" data-test="heading">
         Bookish
       </Typography>
+      <BookList items={books} />
     </div>
   )
 }
